b236f8429860e8ea0fb640b7da6224e0186f0550
sarah <sarahecomp@gmail.com>
2021-02-28 16:54:10 -0300

    Configura o timezone para o Brasil

--- vough_backend/vough_backend/settings.py
+++ vough_backend/vough_backend/settings.py
@@ -114,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "America/Sao_Paulo"
 
 USE_I18N = True
 
@@ -125,7 +125,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
 STATIC_URL = "/static/"
 
 # Activate Django-Heroku.
