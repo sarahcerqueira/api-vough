144f196bc1df8deaa3931208cc642efdab663e0d
sarah <sarahecomp@gmail.com>
2021-03-01 23:12:39 -0300

    Salva e remove organizaçoes

--- vough_backend/api/views.py
+++ vough_backend/api/views.py
@@ -3,6 +3,7 @@ from rest_framework.views import Response
 
 from api import models, serializers
 from api.integrations.github import GithubApi
+from api.models import Organization
 
 # TODOS:
 # 1 - Buscar organização pelo login através da API do Github
@@ -18,4 +19,26 @@ class OrganizationViewSet(viewsets.ModelViewSet):
     lookup_field = "login"
 
     def retrieve(self, request, login=None):
+
+        if login != None:
+            gitapi = GithubApi()
+            result = gitapi.get_organization(login)
+
+            if result != None:
+                org = Organization()
+                org.login = login
+                org.name = result.get('name')
+                org.score = result.get('public_repos') + gitapi.get_organization_public_members(login)
+                org.save()
+                return Response(serializers.OrganizationSerializer(org).data)
+            else:
+                return Response(status=404)
+
         return Response({})
+
+    def destroy(self, request, login=None):
+        org = Organization.objects.filter(login=login).delete()
+        if org[0] != 0:
+            return Response(status=204)
+        
+        return Response(status=404)
