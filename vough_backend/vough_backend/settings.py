86addd0634674dbe4ec7e8616667cb3f6d957823
sarah <sarahecomp@gmail.com>
2021-03-04 00:33:52 -0300

    Configuraçao do env para Heroku

--- vough_backend/vough_backend/settings.py
+++ vough_backend/vough_backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["api-vough.herokuapp.com", "localhost:8000"]
 
 
 # Application definition
